e0fa9595f45e0613bdfb299bd2281b4b95a30758
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-06-05 20:13:08 +0200

    compute demos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import { useEffect } from "react";
 import { runComputeDemo1 } from "./demos/computeDemo1";
+import { runComputeDemo2 } from "./demos/computeDemo2";
 
 export const App = () => {
   useEffect(() => {
     runComputeDemo1();
   }, []);
-  return <div className="bg-blue-200">test</div>;
+  return (
+    <div className="flex flex-col gap-4 p-4 w-96">
+      <button className="btn btn-primary" onClick={() => runComputeDemo1()}>
+        Run compute demo 1
+      </button>
+      <button className="btn btn-primary" onClick={() => runComputeDemo2()}>
+        Run compute demo 2
+      </button>
+    </div>
+  );
 };
 
