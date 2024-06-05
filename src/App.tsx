468a8e4dbb62f10e907dfadbf418cf437dddb008
Mateusz Migas <54471371+mateuszmigas@users.noreply.github.com>
2024-06-05 19:58:14 +0200

    basic compute demo 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+import { runComputeDemo1 } from "./demos/computeDemo1";
+
 export const App = () => {
+  useEffect(() => {
+    runComputeDemo1();
+  }, []);
   return <div className="bg-blue-200">test</div>;
 };
 
